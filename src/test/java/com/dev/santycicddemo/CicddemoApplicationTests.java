1eaec071a2f994dd9b5455cb8e1c894c647b8f8c
santhosh kumar <santhoshkumar@santhoshs-MacBook-Pro.local>
2021-06-29 06:15:27 +0530

    initial commit

---
+++ src/test/java/com/dev/santycicddemo/CicddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.santycicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
